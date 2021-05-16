adbe31047f9635793f06800226b4f276d251c085
coopersimir <47125464+coopersimir@users.noreply.github.com>
2021-05-16 19:02:02 -0400

    Update Find_Actor_Path.java

--- Find_Actor_Path.java
+++ Find_Actor_Path.java
@@ -10,36 +10,33 @@ import Graph_Resources.*;
 public class Find_Actor_Path {
 
 	public static void main(String[] args) {
+		ShortestPath movie_graph = new ShortestPath();
 		try {
-			Reader reader = new FileReader("/Users/simircooper/Desktop/tmdb_5000_credits.csv");
+			Reader reader = new FileReader(args[0]);
 			CSVParser csvParser = new CSVParser(reader, CSVFormat.DEFAULT);
 			JSONParser jsonParser = new JSONParser();
-			Graph<String> movie_graph = new Graph<String>();
 			int movies = 0;
 			for (CSVRecord csvRecord : csvParser) {
-				if(movies==1) {
+				if(movies>=1) {
 					String castJSON = csvRecord.get(2); //read cast in 
 					Object object = jsonParser.parse(castJSON); //parse cast 
 					JSONArray jsonArray = (JSONArray)object; //turn cast into an array
-					for (int i = 0; i < jsonArray.size(); i++) {
-						for(int w=1; i<jsonArray.size()-1;w++) {
-							JSONObject jsonObject1 = (JSONObject)jsonArray.get(i);
-							JSONObject jsonObject2 = (JSONObject)jsonArray.get(w);
-							movie_graph.addEdge((String)jsonObject1.get("name"),(String)jsonObject2.get("name"), true);
-						}
+					//double forloop creates the graph 
+					for (int i = 0; i < jsonArray.size()-1; i++) {
+						JSONObject jsonObject1 = (JSONObject)jsonArray.get(i);
+						JSONObject jsonObject2 = (JSONObject)jsonArray.get(i+1);
+						movie_graph.addEdge((String)jsonObject1.get("name"),(String)jsonObject2.get("name"));
 					}
-					System.out.println(movie_graph.toString());
-				
-						
 				}
 				++movies;
 			}
 			csvParser.close();
 		}
-		catch (Exception e) {
-			System.out.println("File is invalid or is in the wrong format.");
-			
+		
+		catch(Exception e){
+			System.out.println("File "+args[0]+" is invalid or is in the wrong format.");
 		}
+		System.out.println(movie_graph.breadthFirstSearch(movie_graph,"Hugh Jackman", "Patrick Stewart"));
 
 	}
 
